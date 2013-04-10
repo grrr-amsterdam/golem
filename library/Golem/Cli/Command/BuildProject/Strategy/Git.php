9a9075980c86f08840a7857cd3167441b20a4dd4
David Spreekmeester <david@grrr.nl>
2013-04-10 14:47:18 +0200

    meer files in .gitignore

--- library/Golem/Cli/Command/BuildProject/Strategy/Git.php
+++ library/Golem/Cli/Command/BuildProject/Strategy/Git.php
@@ -184,13 +184,18 @@ class Golem_Cli_Command_BuildProject_Strategy_Git implements Golem_Cli_Command_B
 		$ignoreThis .= "application/data/cache/CSS/*\n";
 		$ignoreThis .= "application/data/cache/tags/*\n";
 		$ignoreThis .= "application/configs/version.php\n";
-		$ignoreThis .= "public/cached/*\n";
+		$ignoreThis .= "application/data/logs/*.log";
+		$ignoreThis .= "application/data/sql/*.sql";
+		$ignoreThis .= "chromedriver.log";
+		$ignoreThis .= "public/cached/**/*\n";
 		$ignoreThis .= "public/css/.sass-cache\n";
 		$ignoreThis .= "public/uploads/private/**/*\n";
 		$ignoreThis .= "public/uploads/shared/**/*\n";
+		$ignoreThis .= "public/uploads/sandbox/*";
 		$ignoreThis .= ".DS_Store\n";
 		$ignoreThis .= ".project\n";
 		file_put_contents('.gitignore', $ignoreThis);
 		passthru('git add .gitignore');
+		
 	}
 }
