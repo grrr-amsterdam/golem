87cc978b139fe6a159c31857863175fdb8f603bc
David Spreekmeester <david@grrr.nl>
2013-06-04 15:26:41 +0200

    garp config ini instead of zend

--- library/Golem/Content/Cdn/Distributor.php
+++ library/Golem/Content/Cdn/Distributor.php
@@ -66,7 +66,7 @@ class Golem_Content_Cdn_Distributor {
 	public function distribute($env, $assetList, $assetCount) {
 		$this->_validateEnvironment($env);
 		
-		$ini = new Zend_Config_Ini(APPLICATION_PATH.'/configs/application.ini', $env);
+		$ini = new Garp_Config_Ini(APPLICATION_PATH.'/configs/application.ini', $env);
 
 		if (
 			count($assetList) &&
