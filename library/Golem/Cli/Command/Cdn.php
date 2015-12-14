ef27e4558a901a3831e7e4b695e21d1ca388b86f
Harmen Janssen <harmen@grrr.nl>
2015-12-14 10:42:03 +0100

    Moved Golem Content reference to Garp

--- library/Golem/Cli/Command/Cdn.php
+++ library/Golem/Cli/Command/Cdn.php
@@ -12,15 +12,15 @@ class Golem_Cli_Command_Cdn extends Golem_Cli_Command {
 	const FILTER_DATE_PARAM 			= 'since';
 	const FILTER_DATE_VALUE_NEGATION 	= 'forever';
 	const FILTER_ENV_PARAM 				= 'to';
-	
+
 	const DRY_RUN_PARAM					= 'dry';
-	
+
 	protected $_distributor;
 
 	public function __construct() {
-		$this->_distributor = new Golem_Content_Cdn_Distributor();
+		$this->_distributor = new Garp_Content_Cdn_Distributor();
 	}
-	
+
 	/**
 	 * Distributes the public assets on the local server to the configured CDN servers.
 	 */
@@ -29,7 +29,7 @@ class Golem_Cli_Command_Cdn extends Golem_Cli_Command {
 		$filterDate			= $this->_getFilterDate($args);
 		$filterEnvironments = $this->_getFilterEnvironments($args);
 		$isDryRun			= $this->_getDryRunParam($args);
-			
+
 		$assetList 			= $this->_distributor->select($filterString, $filterDate);
 
 		if (!$assetList) {
@@ -58,24 +58,24 @@ class Golem_Cli_Command_Cdn extends Golem_Cli_Command {
 		Garp_Cli::lineOut("Distributing all assets to the CDN servers:");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute");
 		Garp_Cli::lineOut("");
-		
+
 		Garp_Cli::lineOut("Examples of distributing a specific set of assets to the CDN servers:");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute main.js");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute css");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute css/icons");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute logos");
 		Garp_Cli::lineOut("");
-		
+
 		Garp_Cli::lineOut("Distributing to a specific environment:");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute --to=development");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute main.js --to=staging");
 		Garp_Cli::lineOut("");
-		
+
 		Garp_Cli::lineOut("Default only recently modified files will be distributed.");
 		Garp_Cli::lineOut("To distribute all files:");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute --since=forever");
 		Garp_Cli::lineOut("");
-		
+
 		Garp_Cli::lineOut("To distribute files modified since a specific date (use a 'strtotime' compatible argument):");
 		Garp_Cli::lineOut("\tgarp.php Cdn distribute --since=yesterday");
 		Garp_Cli::lineOut("");
@@ -116,7 +116,7 @@ class Golem_Cli_Command_Cdn extends Golem_Cli_Command {
 			(array)$args[self::FILTER_ENV_PARAM] :
 			$allEnvironments
 		;
-			
+
 		return $environments;
 	}
 
