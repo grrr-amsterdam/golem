cfff15cab80c8217603b7cfd7d72b34398aff2b9
Harmen Janssen <harmen@grrr.nl>
2016-05-19 09:34:23 +0200

    Offer a choice of servers to ssh into

--- library/Golem/Cli/Command/Ssh.php
+++ library/Golem/Cli/Command/Ssh.php
@@ -8,7 +8,7 @@
  * @package      Golem_Cli_Command
  */
 class Golem_Cli_Command_Ssh extends Golem_Cli_Command {
-		
+
 	public function staging() {
 		$this->_openConnection('staging');
 	}
@@ -28,21 +28,50 @@ class Golem_Cli_Command_Ssh extends Golem_Cli_Command {
 			);
 		}
 
-		if (empty($params['server']) || empty($params['user'])) {
-			Garp_Cli::errorOut(
-				"'server' and 'user' are required settings."
-			);
+		if (empty($params['server'])) {
+			Garp_Cli::errorOut("'server' is a required setting.");
 			return false;
 		}
 
-		passthru('ssh ' . escapeshellarg($params['user']) . 
-			'@' . escapeshellarg($params['server']));
+		// To provide a bit of backward-compatibility, convert to array
+		if (!is_array($params['server'])) {
+			$params['server'] = array(
+				array(
+					'server' => $params['server'],
+					'user' => $params['user']
+				)
+			);
+		}
+
+		if (count($params['server']) === 1) {
+			$this->_executeSshCommand($params['server'][0]['server'], $params['server'][0]['user']);
+			return true;
+		}
+
+		$choice = Garp_Cli::prompt(
+			"Choose a server to use: \n" . array_reduce($params['server'],
+				function($output, $server) {
+					$number = count(explode("\n", $output));
+					$output .= "$number) {$server['server']}\n";
+					return $output;
+				}, '')
+		);
+		if (!array_key_exists($choice-1, $params['server'])) {
+			Garp_Cli::errorOut('Invalid choice: ' . $choice);
+			return false;
+		}
+		$this->_executeSshCommand($params['server'][$choice-1]['server'],
+			$params['server'][$choice-1]['user']);
 	}
 
+	protected function _executeSshCommand($server, $user) {
+		passthru('ssh ' . escapeshellarg($user) .
+			'@' . escapeshellarg($server));
+	}
 
 	public function help() {
 		Garp_Cli::lineOut('Usage:');
 		Garp_Cli::lineOut(' g ssh staging', Garp_Cli::BLUE);
 		return true;
-	}		
+	}
 }
